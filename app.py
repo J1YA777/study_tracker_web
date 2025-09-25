dd90d32ee8c30e22c7c6bb42dc71899e14d667ed
Jiya <jiyaeshaal1@gmail.com>
2025-09-25 19:53:19 +0500

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
+
+onn = sqlite3.connect("database.db")
+
+# Create users table
+conn.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT
+)
+""")
+
+# Create scores table
+conn.execute("""
+CREATE TABLE IF NOT EXISTS scores (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    date TEXT,
+    subject TEXT,
+    test_type TEXT,
+    score REAL,
+    max_score REAL,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+)
+""")
+
+conn.close() 
 
 @app.route("/")
 def home():
